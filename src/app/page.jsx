8499c86a2f602d61bce06353ca0fb5a8ba0c5b06
rakhapy <rakhapy@gmail.com>
2026-05-04 19:42:03 +0700

    my last commit.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,10 +7,8 @@ export default function Home() {
   return (
     <div className="flex w-full justify-center gap-1 items-center flex-col min-h-screen sm:p-10">
       <Image src="https://www.svgrepo.com/show/483652/hacker.svg" width={100} height={100} alt="" />
-      <h1 className=" text-2xl font-bold" > This Server is Fucking Down</h1>
-      <h2>Go away from here kid</h2>
-      <Link href={"pages/main"} >Go away</Link>
-      {/* <Button action={() => {redirect("pages/main")}} padding="mt-2 w-[6rem]" > Go Away</Button>   */}
+      <h1 className=" text-center text-5xl font-bold" > This Is a Dead Project</h1>
+      <h2 className="text-md" >Rakha's Project</h2>
     </div>
   );
 }
